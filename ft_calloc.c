8da1cc066fdc4642bd6ef825ffb941ffe58fd401
Jaehun Lee <jaehlee@c2r8s2.42gyeongsan.kr>
2025-04-25 19:25:26 +0900

    libft

--- ft_calloc.c
+++ ft_calloc.c
@@ -10,17 +10,16 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
-#include <stdint.h>
 
 void	*ft_calloc(size_t num, size_t size)
 {
 	void	*ptr;
 
-	if (size != 0 && num > SIZE_MAX / size)
-		return (NULL);
+	if (size != 0 && num > ((size_t)-1) / size)
+		return (0);
 	ptr = malloc(num * size);
 	if (!ptr)
-		return (NULL);
+		return (0);
 	ft_bzero(ptr, num * size);
 	return (ptr);
 }
